1052c0d7782c6c7532169e3e1ea2e453ea90454e
bwpedro <pwarmling0@gmail.com>
2018-09-22 13:43:58 -0300

    add novas funções

--- biblioteca.py
+++ biblioteca.py
@@ -21,17 +21,51 @@ def validaLogin(fileLogin):
     return
 
 def addCarrinho(filePrato, fileLista):
+    print("Os pratos disponíveis são: \n")
+    # lista os pratos
+
+    mais = True
+
+    print("Adicionar ao carrinho: \n")
+    prato = input("Pressione 0 para fechar suas compras.")
+
+    while mais:
+        if prato is "0":
+            mais = False
+        else:
+            fileLista.write(prato)
+
+    print("Pratos adicionados com sucesso!")
 
     return
 
 def consultarLista(fileLista):
-    
+    print("Sua lista possui os seguintes pratos: \n")
+    # listar os pratos da lista
+    gerarLista(fileLista)
+
     return
 
 def gerarLista(fileLista):
+    gerar = True
+    selec = 0
+
+    while gerar:
+        input = ("Deseja gerar sua lista de compras? (1-Sim/2-Não)\n")
+        if selec is 1:
+            # gera a lista
+            gerar = False
+        elif selec is 2:
+            return
+        else:
+            print("Opção não disponível\n")
+    
+    print("Lista gerada com sucesso, consulte ingredientes.txt")
 
     return
 
 def limparCarrinho(fileLista):
-    
+    # limpa a lista
+    print("Carrinho limpo!")
+
     return
